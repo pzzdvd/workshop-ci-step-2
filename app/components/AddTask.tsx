bc54426295b110514f56e3dbea5db9dd6064e104
luca.favaretto <luca.favaretto@addvalue.it>
2025-04-17 15:57:10 +0200

    Push Luca

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,47 +1,48 @@
 "use client";
 
-import { AiOutlinePlus } from "react-icons/ai";
-import { FormEventHandler, useState } from "react";
-import { addTodo } from "@/api";
-import { useRouter } from "next/navigation";
-import { v4 as uuidv4 } from "uuid";
+import {AiOutlinePlus} from "react-icons/ai";
+import {FormEventHandler, useState} from "react";
+import {addTodo} from "@/api";
+import {useRouter} from "next/navigation";
+import {v4 as uuidv4} from "uuid";
 import UpsertTaskModal from "./UpsertTaskModal";
 
 const AddTask = () => {
-  const router = useRouter();
-  const [modalOpen, setModalOpen] = useState<boolean>(false);
-  const [newTaskValue, setNewTaskValue] = useState<string>("");
+    const router = useRouter();
+    const [modalOpen, setModalOpen] = useState<boolean>(false);
+    const [newTaskValue, setNewTaskValue] = useState<string>("");
+    const [newTaskPriority, setNewTaskPriority] = useState<"MEDIUM" | "LOW" | "HIGH">("MEDIUM");
 
-  const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
-    e.preventDefault();
-    await addTodo({
-      id: uuidv4(),
-      text: newTaskValue,
-    });
-    setNewTaskValue("");
-    setModalOpen(false);
-    router.refresh();
-  };
+    const handleSubmitNewTodo: FormEventHandler<HTMLFormElement> = async (e) => {
+        e.preventDefault();
+        await addTodo({
+            id: uuidv4(), text: newTaskValue, priority: newTaskPriority
+        });
+        setNewTaskValue("");
+        setModalOpen(false);
+        setNewTaskPriority("MEDIUM");
+        router.refresh();
+    };
 
-  return (
-    <div>
-      <button
-        data-testid="add-todo"
-        onClick={() => setModalOpen(true)}
-        className='btn btn-primary w-full'
-      >
-        Add new task <AiOutlinePlus className='ml-2' size={18} />
-      </button>
+    return (<div>
+        <button
+            data-testid="add-todo"
+            onClick={() => setModalOpen(true)}
+            className='btn btn-primary w-full'
+        >
+            Add new task <AiOutlinePlus className='ml-2' size={18}/>
+        </button>
 
-      <UpsertTaskModal 
-        modalOpen={modalOpen} 
-        setModalOpen={setModalOpen}
-        handleSubmit={handleSubmitNewTodo}
-        value={newTaskValue}
-        setValue={setNewTaskValue}
-      />
-    </div>
-  );
+        <UpsertTaskModal
+            modalOpen={modalOpen}
+            setModalOpen={setModalOpen}
+            handleSubmit={handleSubmitNewTodo}
+            value={newTaskValue}
+            setValue={setNewTaskValue}
+            priority={newTaskPriority}
+            setPriority={setNewTaskPriority}
+        />
+    </div>);
 };
 
 export default AddTask;
--- app/components/UpsertTaskModal.tsx
+++ app/components/UpsertTaskModal.tsx
@@ -1,36 +1,58 @@
-import { FormEventHandler } from "react";
+import {FormEventHandler} from "react";
 import Modal from "./Modal";
+import {useSearchParams} from "next/navigation";
 
 interface ModalProps {
-  modalOpen: boolean;
-  setModalOpen: (open: boolean) => boolean | void;
-  handleSubmit: FormEventHandler<HTMLFormElement>;
-  value: string;
-  setValue: (value: string) => boolean | void;
-  variant?: 'add' | 'edit';
+    modalOpen: boolean;
+    setModalOpen: (open: boolean) => boolean | void;
+    handleSubmit: FormEventHandler<HTMLFormElement>;
+    value: string;
+    setValue: (value: string) => boolean | void;
+    priority?: 'MEDIUM' | 'LOW' | 'HIGH';
+    setPriority?: (priority: 'MEDIUM' | 'LOW' | 'HIGH') => boolean | void;
+    variant?: 'add' | 'edit';
 }
 
-const UpsertTaskModal: React.FC<ModalProps> = ({ modalOpen, setModalOpen, value, setValue, handleSubmit, variant = 'add' }) => {
-  return (
-    <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+
+const UpsertTaskModal: React.FC<ModalProps> = ({
+                                                   modalOpen,
+                                                   setModalOpen,
+                                                   value,
+                                                   setValue,
+                                                   handleSubmit,
+                                                   variant = 'add',
+                                                   priority = 'MEDIUM',
+                                                   setPriority = () => {
+                                                   }
+                                               }) => {
+    const searchParams = useSearchParams();
+    const isPriorityAbilitato = searchParams.get('show_priority') === '1';
+
+    return (<Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
         <form onSubmit={handleSubmit}>
-          <h3 className='font-bold text-lg'>{variant === 'add' ? 'Add new task' : 'Edit task'}</h3>
-          <div className='modal-action'>
-            <input
-              value={value}
-              onChange={(e) => setValue(e.target.value)}
-              type='text'
-              placeholder='Type here'
-              className='input input-bordered w-full'
-              data-testid={`todo-text-${variant}`}
-            />
-            <button type='submit' className='btn' data-testid={`save-${variant}-todo`}>
-              Submit
-            </button>
-          </div>
+            <h3 className='font-bold text-lg'>{variant === 'add' ? 'Add new task' : 'Edit task'}</h3>
+            <div className='modal-action'>
+                <input
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                    type='text'
+                    placeholder='Type here'
+                    className='input input-bordered w-full'
+                    data-testid={`todo-text-${variant}`}
+                />
+                {isPriorityAbilitato && <select value={priority}
+				                                onChange={(e) => setPriority(e.target.value as 'MEDIUM' | 'LOW' | 'HIGH')}
+				>
+					<option value='LOW'>LOW</option>
+					<option value='MEDIUM'>MEDIUM</option>
+					<option value='HIGH'>HIGH</option>
+				</select>}
+                <button type='submit' className='btn' data-testid={`save-${variant}-todo`}>
+                    Submit
+                </button>
+            </div>
         </form>
-      </Modal>
-  );
+    </Modal>);
 };
 
 export default UpsertTaskModal;
